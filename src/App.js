360e0168f19959d751dadfe38c28703048a84b57
tan loc <tanloc6998@gmail.com>
2021-03-10 17:32:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Homework from "./homework/homework";
+
+function App() {
+  return <Homework />;
+}
+
+export default App;
---
+++ src/homework/homework.js
@@ -0,0 +1,19 @@
+import Header from "./header";
+import Carousel from "./carousel";
+import Smartphone from "./smartphone";
+import Laptop from "./laptop";
+import Promotion from "./promotion";
+
+function Homework() {
+  return (
+    <div>
+      <Header />
+      <Carousel />
+      <Smartphone />
+      <Laptop />
+      <Promotion />
+    </div>
+  );
+}
+
+export default Homework;
